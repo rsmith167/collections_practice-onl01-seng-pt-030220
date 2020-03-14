d72bc5895fb5dd114d6ffc64e8f4683649ee058b
Rwanda S <rwanda.ace@gmail.com>
2020-03-14 16:39:49 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,52 @@
-
+def sort_array_asc(array)
+  array.sort
+end
+def sort_array_desc(array)
+  array.sort!
+  array.reverse!
+end
+def sort_array_char_count(array)
+  array.sort do |a,b| a.length <=> b.length
+end
+end
+def swap_elements(array)
+ placeholder = []
+ placeholder[0] = array[1]
+ array[1] = array [2]
+ array[2] = placeholder[0]
+ array
+end
+def reverse_array(array)
+  array.reverse
+end
+def kesha_maker(array)
+  new_array = []
+  array.each do |word|
+    word[2] = "$"
+    new_array << word
+  end
+  new_array
+end
+def find_a(array)
+array.select do |word| word.start_with?("a")
+end
+end
+def sum_array(array)
+  i= 0
+  sum = 0
+  while i < array.length
+  sum = sum + array[i]
+  i+=1
+end
+sum
+  
+end
+def add_s(array)
+  array.each_with_index.collect do |word, index|
+    if index != 1
+      word << "s"
+    else 
+      word
+    end
+  end
+end

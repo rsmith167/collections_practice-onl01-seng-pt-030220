3a84c02f22ab563a9657b1e391963bdd58966510
Rwanda S <rwanda.ace@gmail.com>
2020-03-14 16:26:26 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,14 @@
-
+def sort_array_asc(array)
+  array.sort
+end
+def sort_array_desc(array)
+  array.sort!
+  array.reverse!
+end
+def sort_array_char_count(array)
+  array.sort do |a,b| a.length <=> b.length
+end
+end
+def swap_elements(array)
+  
+end

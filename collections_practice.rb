c9fdb2a1877cfb0063ab7577c0b89b8cdbd43e5b
Rwanda S <rwanda.ace@gmail.com>
2020-03-14 16:30:39 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -10,5 +10,21 @@ def sort_array_char_count(array)
 end
 end
 def swap_elements(array)
-  
+ placeholder = []
+ placeholder[0] = array[1]
+ array[1] = array [2]
+ array[2] = placeholder[0]
+end
+def reverse_array(array)
+  array.reverse
+end
+def kesha_maker(array)
+  new_array = []
+  array.each do |word|
+    word[2] = "!"
+    new_array << word
+  end
+end
+def find_a(array)
+
 end
